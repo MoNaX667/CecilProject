5faf500567eb1fee976998593626b2677d311c6b
Vitaliy Belyakov <MoNaX_667@mail.ru>
2016-05-10 20:35:15 +0300

    FirstLoad
    ConnectDll

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Mono.Cecil;
+using Mono.Cecil.Cil;
+
+namespace CecilProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
